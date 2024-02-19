509d16564b572081c6bf0d3a93df472a30659c6c
youssouf1994 <youssouf.emin94@gmail.com>
2024-02-19 18:30:31 -0500

    add animated circle.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from textwrap import fill
 import manim as mn
 
-class Circle(mn.Scene):
+class AnimatedCircle(mn.Scene):
     def construct(self):
-        circle = mn.Circle()
-        self.add(circle)
+        circle = mn.Circle(
+            radius=2,
+            fill_color=mn.BLUE,
+            fill_opacity=0.5,
+            stroke_color=mn.RED,
+        )
+        text = mn.Text("Voici un cercle", font_size=24).next_to(circle, mn.UP)
+        self.play(mn.Create(circle), mn.Write(text))
