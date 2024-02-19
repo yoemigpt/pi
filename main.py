b90bdd016e7afabd2ee3c924b9a6a987ea03e53b
youssouf1994 <youssouf.emin94@gmail.com>
2024-02-19 18:39:03 -0500

    add animated pi

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from textwrap import fill
 import manim as mn
+
+class AnimatedPI(mn.Scene):
+    def construct(self):
+        pi = mn.Tex(
+            "$\pi$",
+            font_size=256,
+            color=mn.RED
+        )
+        self.play(mn.Write(pi))
+        
+        text = mn.Tex(
+            "3.141592653589793238462643383279502",
+            font_size=24
+        ).next_to(pi, mn.DOWN)
+        self.play(mn.Write(text))
 
 class AnimatedCircle(mn.Scene):
     def construct(self):
